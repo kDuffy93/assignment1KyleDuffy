a77588d42d2204fe1084bd1c1ebf75ea1bbbe4ab
KyleDuffy93 <kyleduffy_83@Hotmail.com>
2017-02-16 06:53:19 -0500

    finished adding content and comments

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
+<!--  GET about page  -->
 router.get('/about', function(req, res, next) {
     res.render('about', { title: 'Express' });
 });
+<!-- GET contact page   -->
 router.get('/contact', function(req, res, next) {
     res.render('contact', { title: 'Express' });
 });
+<!--  GET  projects page  -->
 router.get('/projects', function(req, res, next) {
     res.render('projects', { title: 'Express' });
 });
+<!-- GET services page   -->
 router.get('/services', function(req, res, next) {
     res.render('services', { title: 'Express' });
 });
